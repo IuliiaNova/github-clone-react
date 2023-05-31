19127fe9b8c1c088f3cb215dc853b925e35a4242
Iuliia Shikhanova <yshihanova@gmail.com>
2023-05-31 23:48:41 +0100

    create: sidebar + style

--- src/view/components/Sidebar/Sidebar.tsx
+++ src/view/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,17 @@
 import './sidebar.scss';
-import React, { useState } from 'react';
+import { useState } from 'react';
+import img from '../../../assets/img/avatar.png'
 
 const Sidebar = () => {
   const [toggleDropdown, setToggleDropdown] = useState(false)
 
-
   
   return (
 
-      <nav className='header__nav'>
-        <img src='@@' alt='User avatar'/>
-        <span>Nickname</span>
-        <button onClick={() => setToggleDropdown(!toggleDropdown)}>Edir profile</button>
+      <nav className='sidebar__nav'>
+        <img src={img} alt='User avatar' className='sidebar__nav--img'/>
+        <span className='sidebar__nav--span'>Nickname</span>
+        <button onClick={() => setToggleDropdown(!toggleDropdown)} className='sidebar__nav--btn'>Edit profile</button>
 
         {toggleDropdown ? (
           <div>hola</div>
