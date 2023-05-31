8287acd70bce7bd50276dfcd8be816ec2a44ef91
Iuliia Shikhanova <yshihanova@gmail.com>
2023-05-31 23:48:59 +0100

    create: header + style

--- src/view/components/Header/Header.tsx
+++ src/view/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import './header.scss';
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import { RiStarSLine, RiBook2Line, RiProfileLine } from "react-icons/ri";
@@ -7,15 +6,15 @@ import { RiStarSLine, RiBook2Line, RiProfileLine } from "react-icons/ri";
 
 const Header = () => {
   const location = useLocation();
-  
+
   return (
     <header className='header'>
       <nav className='header__nav'>
         <ul className='header__nav__list'>
-          <Link to='/'><li className={`navbar-page__nav__list__item ${location.pathname === '/' ? 'active' : ''}`}><RiProfileLine/>Overview</li></Link>
-          <Link to='repos'><li className={`navbar-page__nav__list__item ${location.pathname === '/repos' ? 'active' : ''}`}><RiBook2Line/>Repositories</li></Link>
-          <Link to='stars'><li className={`navbar-page__nav__list__item ${location.pathname === '/stars' ? 'active' : ''}`}><RiStarSLine/>Stars</li></Link>
-          </ul>
+          <Link to='/'><li className={`header__nav__list__item ${location.pathname === '/' ? 'active' : ''}`}><RiProfileLine className="icon"/>Overview</li></Link>
+          <Link to='repos'><li className={`header__nav__list__item ${location.pathname === '/repos' ? 'active' : ''}`}><RiBook2Line className="icon"/>Repositories</li></Link>
+          <Link to='stars'><li className={`header__nav__list__item ${location.pathname === '/stars' ? 'active' : ''}`}><RiStarSLine className="icon"/>Stars</li></Link>
+        </ul>
       </nav>
     </header>
   )
