f5049afe566883d4dbaa59eaf13a02d44b6fc8aa
Iuliia Shikhanova <yshihanova@gmail.com>
2023-06-04 03:11:17 +0100

    feat(typescript): safe all interfaces saparetly

--- src/api/repositories/repo.api.tsx
+++ src/api/repositories/repo.api.tsx
@@ -1,12 +1,12 @@
 
-export const getAllReposAPI = async (): Promise<any> => {
+export const getAllReposAPI = async (): Promise<unknown> => {
 
 	const response = await fetch('https://api.github.com/users/IuliiaNova/repos')
 	const data = await response.json()
 	return data
 }
 
-export const getReposAPI = async (): Promise<any> => {
+export const getReposAPI = async (): Promise<unknown> => {
 	const response = await fetch('http://localhost:4000/api/v1/get-repos')
 	const data = await response.json()
 	return data
